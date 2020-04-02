7c78ccfed7fbd6bc3d9cd855793e87b338326b1b
Lucien Brun <lucien.brun@epitech.eu>
2020-04-02 18:22:03 +0200

    average return time

--- src/ducks/ducks.go
+++ src/ducks/ducks.go
@@ -3,17 +3,33 @@ package ducks
 import (
 	"continuous"
 	"fmt"
-	//"math"
+	"math"
 )
+
+func formatMinutesToHuman(minutes float64) string {
+	var res string
+
+	hh := continuous.ComputeExpectedValue(minutes) / 60
+	_, hhRHS := math.Modf(hh);
+	mm := hhRHS * 60
+	mmLHS, mmRHS := math.Modf(mm);
+	res += fmt.Sprintf("%.0f", mmLHS)
+	res += "m "
+	
+	ss := mmRHS * 60
+	ssLHS, _ := math.Modf(ss);
+	res += fmt.Sprintf("%.0f", ssLHS + 1)
+	res += "s"
+
+	return res
+}
 
 // Ducks - main function
 func Ducks() {
-	// ceil := int(continuous.ComputeExpectedValue(A))
-	// sec := math.Ceil(continuous.ComputeExpectedValue(A) - float64(ceil) * 60)
-	fmt.Println("Average return time: ", continuous.ComputeExpectedValue(A))
-	fmt.Println("Standard deviation: ", continuous.ComputeStandardDeviation())
-	fmt.Println("Time after which 50% of the ducks are back: ", continuous.ComputeTimeDucksBack(50))
-	fmt.Println("Time after which 99% of the ducks are back: ", continuous.ComputeTimeDucksBack(99))
-	fmt.Println("Percentage of ducks back after 1 minute: ", continuous.ComputePercentageDucksBack(1))
-	fmt.Println("Percentage of ducks back after 2 minutes: ", continuous.ComputePercentageDucksBack(2))
+	fmt.Println("Average return time:", formatMinutesToHuman(A))
+	fmt.Println("Standard deviation:", continuous.ComputeStandardDeviation())
+	fmt.Println("Time after which 50% of the ducks are back:", continuous.ComputeTimeDucksBack(50))
+	fmt.Println("Time after which 99% of the ducks are back:", continuous.ComputeTimeDucksBack(99))
+	fmt.Println("Percentage of ducks back after 1 minute:", continuous.ComputePercentageDucksBack(1))
+	fmt.Println("Percentage of ducks back after 2 minutes:", continuous.ComputePercentageDucksBack(2))
 }
